9f78907478d66aa599d257484e06da9ae03ffea6
mvcobos <vcobos178@gmail.com>
2024-11-13 12:46:10 -0500

    Arreglos

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        Arreglos arreglo = new Arreglos();
+        System.out.println("Arreglo de 10");
+        arreglo.generarArreglo10();
+        System.out.println("Arreglo de 100");
+        int[] arreglo10 = arreglo.generarArreglo10();
+        arreglo.generarArreglo100(arreglo10);
     }
 }
